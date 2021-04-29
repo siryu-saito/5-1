2a1921a0c5014a2256f2e458618664f9dd07cbe3
siryu-saito <saito.s.ryu@gmail.com>
2021-04-29 09:53:08 +0000

    [Finish]5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '23de16b4c160793e544e6ad44701dc65e3f8a7343e48f0baaeaf3e911db289745e0001023d2dc922ba7c6120240cca567da70eb2cd9789654ad70612dacf5305'
